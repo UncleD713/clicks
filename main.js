ecaf6dd037d7742711fed2d714ff5d26343e026f
Derrick Bell <dbell34@mail.valenciacollege.edu>
2021-03-24 12:07:36 -0400

    initial commit

--- main.js
+++ main.js
@@ -14,24 +14,28 @@ for (let i = 0; i < buttons.length; i++) {
 
 const main = document.createElement("main");
 const button = document.querySelector("button");
+const uuidv5 = require('uuid/v5');
 
 button.addEventListener("click", responseToClick) 
         function responseToClick () {
+        id: uuidv4("a55ae4a3-89d1-453f-afeb-395178976be0");
         const h3 = document.createElement ("h3");
         h3.textContent = "Foo"; 
         main.appendChild(h3); 
+        
     }
 
 button.addEventListener("click", responseToClick)
         function responseToClick () {
+        id: uuidv4("ccf1df3e-be75-4428-9c03-3ea7bc80bd64");
         const h3 = document.createElement ("h3");
         h3.textContent = "Bar"; 
         main.appendChild(h3);
      }
     
-
     button.addEventListener("click", responseToClick) 
         function responseToClick() {
+        id: uuidv4("bc9843d1-60a0-421b-b7df-410ea2e132c1");
         const h2 = document.createElement("h2");
         h2.textContent = "FooBar";
         main.appendChild(h2);
